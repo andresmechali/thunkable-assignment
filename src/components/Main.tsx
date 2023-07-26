e53ee476110339596c0ef87cf1cdf870031722e3
Andres Mechali <andresmechali@gmail.com>
2023-07-26 17:01:32 +0200

    fix overflow

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,7 +7,7 @@ const Main: FC = () => {
   const ui = useAppSelector((state) => state.ui);
 
   return (
-    <main className="w-full flex flex-col justify-start items-center pt-[71px] md:pt-[60px]">
+    <main className="w-full flex flex-col justify-start items-center pt-[71px] md:pt-[60px] overflow-auto">
       {ui.creating && <NewProject />}
       <ProjectList />
     </main>
